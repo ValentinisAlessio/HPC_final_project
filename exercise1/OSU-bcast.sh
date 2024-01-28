065beaf0f2b7535fc87f01ec7877f9e1b6fdd4f9
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-01-28 17:16:08 +0100

    Now should be correct

--- exercise1/OSU-bcast.sh
+++ exercise1/OSU-bcast.sh
@@ -7,8 +7,9 @@
 #SBATCH --ntasks=256
 #SBATCH -p EPYC
 #SBATCH --exclusive
-#SBATCH --time=01:00:00
+#SBATCH --time=02:00:00
 #SBATCH --mail-type=ALL
+#SBATCH --mail-user=ALESSIO.VALENTINIS@studenti.units.it
 
 # Load the MPI module
 module load openMPI/4.1.5/gnu/12.2.1
@@ -17,7 +18,7 @@ module load openMPI/4.1.5/gnu/12.2.1
 csv_file="results/bcast_results.csv"
 
 # Go to the directory where the benchmark is located
-cd ../../osu-micro-benchmarks-7.3/c/mpi/collective/blocking/
+src_path="../../osu-micro-benchmarks-7.3/c/mpi/collective/blocking/"
 
 
 # Define variables
@@ -31,7 +32,7 @@ bcast_algorithm="0 1 2 3"  # Example broadcast algorithms
 map_values="core socket node"
 
 # Create CSV file and add headers
-echo "Algorithm,Allocation,Processes,MessageSize,Avg Latency(us)" > csv_file
+echo "Algorithm,Allocation,Processes,MessageSize,Avg Latency(us)" > $csv_file
 
 # Loop through process map_values
 for mapping in $map_values; do
@@ -41,9 +42,10 @@ for mapping in $map_values; do
         for broadcast_algo in $bcast_algorithm; do
             echo "Running MPI Bcast benchmark: map=$mapping, np=$np, broadcast_algo=$broadcast_algo ..."
             # Run MPI Bcast benchmark and capture output
-            output=$(mpirun -np $np --map-by $mapping --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm $broadcast_algo osu_bcast)
+            output=$(mpirun -np $np --map-by $mapping --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm $broadcast_algo $src_path/osu_bcast)
             # Append results to CSV file
-            echo "$output" | awk -v mapping="$mapping" -v np="$np" -v algo="$broadcast_algo" 'NR>2 {print algo "," mapping "," np "," $1 "," $2}' >> $csv_file
+            tail -n 21 "$output" | awk -v mapping="$mapping" -v np="$np" -v algo="$broadcast_algo" '{printf "%s,%s,%s,%s,%s\n",broadcast_algo,mapping,np,$1,$2}'\
+            | sed 's/,$//' >> $csv_file
         done
     done
 done
