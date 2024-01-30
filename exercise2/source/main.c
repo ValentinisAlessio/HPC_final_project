dc51ebfa13f5e1c05bf67f1b64b7f598756fca27
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-01-30 16:38:10 +0100

    First try on quicksort

--- exercise2/source/main.c
+++ exercise2/source/main.c
@@ -90,7 +90,9 @@ int main(int argc, char** argv){
     // Compute chunk size to be sent to each process
     // int chunk_size = (N % num_processes == 0) ? N / num_processes : N / (num_processes - 1);
     int chunk_size = N / num_processes;
-
+    if (rank == 0) {
+        printf("Chunk size: %d\n", chunk_size)
+    }
     // Allocate memory for the chunk
     data_t* chunk = (data_t*)malloc(chunk_size*sizeof(data_t));
 
