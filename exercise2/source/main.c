64e70ae1f2f5ec7ba37a79bdcf3e070db8e3e879
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-01-30 16:43:33 +0100

    Debugging messages

--- exercise2/source/main.c
+++ exercise2/source/main.c
@@ -132,7 +132,7 @@ int main(int argc, char** argv){
     // using quick sort
 
     if (rank == 0):
-        printf("Chunk size: %d\n", chunk_size)
+        printf("Chunk size: %d\n", chunk_size);
     int own_chunk_size = (rank < N % num_processes) ? chunk_size+1 : chunk_size;
     for (int i=0; i<num_processes; i++) {
         if (rank == i) {
