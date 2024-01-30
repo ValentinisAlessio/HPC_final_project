b2c22dc599ebd30ae362a6ad0a211194bb714bc2
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-01-30 15:08:37 +0100

    .

--- exercise2/headers/par_quicksort.h
+++ exercise2/headers/par_quicksort.h
@@ -102,6 +102,6 @@ verify_t show_array;
 int partition(data_t *, int, int, compare_t);
 void quicksort(data_t *, int, int, compare_t);
 
-void par_quicksort(data_t *, int, int, compare_t, int);
+void par_quicksort(data_t *, int, int, compare_t);
 
 data_t* merge(data_t *, int, data_t *, int, compare_t);
