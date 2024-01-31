d19dafa3af1ef297bb4155ec69c9d4abc65429f6
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-01-31 15:11:06 +0100

    Created sbatch files for thin nodes

---
+++ exercise1/thin/OSU-bcast-default-thin.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Script to automatize OSU-benchmark routines for boocking broadcast operation, varying the number of processes and the message size
+#SBATCH --job-name=hpcex1
+#SBATCH --output=OSU-def-bcast-thin.out
+#SBATCH --error=OSU-def-bcast-thin.err
+#SBATCH --nodes=2
+#SBATCH --ntasks=72
+#SBATCH -p THIN
+#SBATCH --exclusive
+#SBATCH --time=02:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=ALESSIO.VALENTINIS@studenti.units.it
+
+# Load the MPI module
+module load openMPI/4.1.5/gnu/12.2.1
+
+# Specify the path to the result file
+csv_file="results/bcast_def_results.csv"
+
+# Go to the directory where the benchmark is located
+src_path="../../../osu-micro-benchmarks-thin/c/mpi/collective/blocking/"
+
+
+# Define variables
+#MESSAGE_SIZES=(1024 2048 4096 8192)  # Example message sizes
+
+# Define different process map_values to evaluate
+map_values="core socket node"
+
+# Create CSV file and add headers
+echo "Algorithm,Allocation,Processes,MessageSize,Avg Latency(us)" > $csv_file
+
+# Loop through process map_values
+for mapping in $map_values; do
+    # Loop through number of processes
+    for np in {1...72}; do
+        echo "Running MPI Bcast benchmark: map=$mapping, np=$np, broadcast_algo=default ..."
+        # Run MPI Bcast benchmark and capture output
+        output=$(mpirun -np $np --map-by $mapping --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 0 $src_path/osu_bcast -x 1000 -i 10000)
+        # Append results to CSV file
+        tail -n 21 "$output" | awk -v mapping="$mapping" -v np="$np" '{printf "default,%s,%s,%s,%s\n",mapping,np,$1,$2}'\
+        | sed 's/,$//' >> $csv_file
+    done
+done
