b34f60c1c4b850946b8129a9a841895308b86b6f
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-02-02 15:06:44 +0000

    Results

--- exercise1/thin/OSU-bcast-chain-thin.sh
+++ exercise1/thin/OSU-bcast-chain-thin.sh
@@ -41,7 +41,7 @@ for mapping in $map_values; do
     for np in {1..48}; do
         echo "Running MPI Bcast benchmark: map=$mapping, np=$np, broadcast_algo=$broadcast_algo ..."
         # Run MPI Bcast benchmark and capture output
-        mpirun -np $np --map-by $mapping --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 2 $src_path/osu_bcast -x 100 -i 10000
+        mpirun -np $np --map-by $mapping --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 2 $src_path/osu_bcast -x 100 -i 10000 |\
         # Append results to CSV file
         tail -n 21 | awk -v mapping="$mapping" -v np="$np" '{printf "chain,%s,%s,%s,%s\n",mapping,np,$1,$2}'\
         | sed 's/,$//' >> $csv_file
