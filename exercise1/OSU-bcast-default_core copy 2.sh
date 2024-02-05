f25ab83fab85d957019b7e69af8a7047b2619b05
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-02-05 18:01:43 +0100

    Separated data gathering

--- exercise1/OSU-bcast-default_core copy 2.sh
+++ exercise1/OSU-bcast-default_core copy 2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Script to automatize OSU-benchmark routines for boocking broadcast operation, varying the number of processes and the message size
 #SBATCH --job-name=hpcex1
-#SBATCH --output=bcast-bintree-core.out
-#SBATCH --error=bcast-bintree-core.err
+#SBATCH --output=bcast-def-core.out
+#SBATCH --error=bcast-def-core.err
 #SBATCH --nodes=2
 #SBATCH --ntasks=256
 #SBATCH -p EPYC
@@ -15,7 +15,7 @@
 module load openMPI/4.1.5/gnu/12.2.1
 
 # Specify the path to the result file
-csv_file="results_def/bcast_bintree_results_wu_core.csv"
+csv_file="results_def/bcast_def_results_wu_core.csv"
 
 # Go to the directory where the benchmark is located
 src_path="../../osu-micro-benchmarks-7.3/c/mpi/collective/blocking/"
@@ -23,8 +23,6 @@ src_path="../../osu-micro-benchmarks-7.3/c/mpi/collective/blocking/"
 
 # Define variables
 #MESSAGE_SIZES=(1024 2048 4096 8192)  # Example message sizes
-# np_values=$(seq 2 1 256) # Example number of processes
-
 
 # Define different process map_values to evaluate
 # map_values="core socket node"
@@ -34,9 +32,9 @@ echo "Algorithm,Allocation,Processes,MessageSize,Avg Latency(us)" > $csv_file
 
 # Loop through number of processes
 for np in {1..256}; do
-    echo "Running MPI Bcast benchmark: map=$mapping, np=$np, broadcast_algo=$broadcast_algo ..."
+    echo "Running MPI Bcast benchmark: map=$mapping, np=$np, broadcast_algo=default ..."
     # Run MPI Bcast benchmark and capture output
-    mpirun -np $np --map-by core --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 5 $src_path/osu_bcast -m 2048 -x 100 -i 10000 |\
+    mpirun -np $np --map-by core --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 0 $src_path/osu_bcast -m 2048 -x 100 -i 10000 |\
     # Append results to CSV file
-    tail -n 12 | awk -v np="$np" '{printf "bin_tree,core,%s,%s,%s\n",np,$1,$2}' | sed 's/,$//' >> $csv_file
+    tail -n 21 | awk -v np="$np" '{printf "default,core,%s,%s,%s\n",np,$1,$2}' | sed 's/,$//' >> $csv_file
 done
