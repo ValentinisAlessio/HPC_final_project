23827a68ec52b99b9caf6d173740a5b8d42b2319
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-02-05 17:44:00 +0100

    Separated data gathering

--- exercise1/OSU-bcast-chain_core.sh
+++ exercise1/OSU-bcast-chain_core.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Script to automatize OSU-benchmark routines for boocking broadcast operation, varying the number of processes and the message size
 #SBATCH --job-name=hpcex1
-#SBATCH --output=OSU-def-bcast.out
-#SBATCH --error=OSU-def-bcast.err
+#SBATCH --output=bcast-chain-core.out
+#SBATCH --error=chain-core.err
 #SBATCH --nodes=2
 #SBATCH --ntasks=256
 #SBATCH -p EPYC
@@ -15,7 +15,7 @@
 module load openMPI/4.1.5/gnu/12.2.1
 
 # Specify the path to the result file
-csv_file="results/bcast_def_results_wu_1.csv"
+csv_file="results_def/bcast_chain_results_wu_core.csv"
 
 # Go to the directory where the benchmark is located
 src_path="../../osu-micro-benchmarks-7.3/c/mpi/collective/blocking/"
@@ -23,22 +23,18 @@ src_path="../../osu-micro-benchmarks-7.3/c/mpi/collective/blocking/"
 
 # Define variables
 #MESSAGE_SIZES=(1024 2048 4096 8192)  # Example message sizes
-np_values=(2 $(seq 8 8 256))  # Example number of processes
 
 # Define different process map_values to evaluate
-map_values="core socket node"
+# map_values="core socket node"
 
 # Create CSV file and add headers
 echo "Algorithm,Allocation,Processes,MessageSize,Avg Latency(us)" > $csv_file
 
-# Loop through process map_values
-for mapping in $map_values; do
-    # Loop through number of processes
-    for np in $np_values; do
-        echo "Running MPI Bcast benchmark: map=$mapping, np=$np, broadcast_algo=default ..."
-        # Run MPI Bcast benchmark and capture output
-        mpirun -np $np --map-by $mapping --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 0 $src_path/osu_bcast -x 100 -i 10000 |\
-        # Append results to CSV file
-        tail -n 21 | awk -v mapping="$mapping" -v np="$np" '{printf "default,%s,%s,%s,%s\n",mapping,np,$1,$2}' | sed 's/,$//' >> $csv_file
-    done
+# Loop through number of processes
+for np in {1..256}; do
+    echo "Running MPI Bcast benchmark: map=$mapping, np=$np, broadcast_algo=$broadcast_algo ..."
+    # Run MPI Bcast benchmark and capture output
+    mpirun -np $np --map-by $mapping --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 2 $src_path/osu_bcast -x 100 -i 10000 |\
+    # Append results to CSV file
+    tail -n 21 | awk -v np="$np" '{printf "chain,core,%s,%s,%s\n",np,$1,$2}' | sed 's/,$//' >> $csv_file
 done
