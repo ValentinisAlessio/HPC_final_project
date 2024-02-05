a5a00b1f49e0b445a794da3b0d2cd9798b399e0a
ValentinisAlessio <ALESSIO.VALENTINIS@studenti.units.it>
2024-02-05 11:24:25 +0100

    Typo

--- exercise1/OSU-bcast-bintree.sh
+++ exercise1/OSU-bcast-bintree.sh
@@ -23,8 +23,12 @@ src_path="../../osu-micro-benchmarks-7.3/c/mpi/collective/blocking/"
 
 # Define variables
 #MESSAGE_SIZES=(1024 2048 4096 8192)  # Example message sizes
-np_values=(2)  # Example number of processes
-np_values+=($(seq 8 8 256))  # Example number of processes
+np_values=()
+np_values+=(2)  # Include 2 initially
+for ((i = 8; i <= 256; i += 8)); do
+    np_values+=($i)
+done
+
 
 # Define different process map_values to evaluate
 map_values="core socket node"
